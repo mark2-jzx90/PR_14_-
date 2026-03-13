ed7c28215e4ef3615c5c408b9b8606e2696b3bb4
mark2-jzx90 <maksimzuev300@gmail.com>
2026-03-14 00:44:08 +0500

    Add files via upload

---
+++ Pr_14_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr_14_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("N = ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n <= 0)
+                {
+                    Console.WriteLine("Неверное значение");
+                    Console.ReadLine();
+                    return;
+                }
+                Stack <int> a = new Stack <int>(n);
+                for (int i = 1; i <= n; i++)
+                {
+                    a.Push(i);
+                }
+                Console.WriteLine($"Размерность стека: {a.Count}");
+                Console.WriteLine($"Верхний элемент стека: {a.Peek()}");
+                Console.WriteLine($"Размерность стека: {a.Count}");
+                Console.Write("Содержимое стека: ");
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(a.Pop() + " ");
+
+                }
+                Console.WriteLine($"\nНовый размер стека {a.Count}");
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Неверный формат данных");
+            }
+        }
+    }
+    
+}
---
+++ Pr_14_2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr_14_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                string file1 = "tetx1.txt";
+                string file2 = "text2.txt";
+                Stack<int> stack = new Stack<int>();
+                List<int> closingskobka = new List<int>();
+                for(int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    char c = textBox1.Text[i];
+                    if (c == '(')
+                    {
+                        stack.Push(i);
+                    }
+                    else if(c == ')')
+                    {
+                        if(stack.Count > 0)
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            closingskobka.Add(i);
+                        }
+                    }
+                    
+                }
+                if(stack.Count == 0 && closingskobka.Count == 0)
+                {
+                    MessageBox.Show("Скобки сбалансированы");
+                }
+                else
+                {
+                    MessageBox.Show("Скобки не сбалансированы");
+
+                    foreach(int pos in stack)
+                    {
+                        string proverka = textBox1.Text.Substring(0, pos) + " < " + textBox1.Text[pos] + " > " + textBox1.Text.Substring(pos + 1);
+                        MessageBox.Show($"Лишняя скобка  впозции {pos+1}. Выражение: {proverka}");
+                    }
+                    foreach (int pos in closingskobka)
+                    {
+                        string proverka = textBox1.Text.Substring(0, pos) + " < " + textBox1.Text[pos] + " > " + textBox1.Text.Substring(pos + 1);
+                        MessageBox.Show($"Лишняя скобка  впозции {pos + 1}. Выражение: {proverka}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Напиши выражение");
+            }
+            File.WriteAllText("text1.txt",textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                Stack<int> stack = new Stack<int>();
+                List<int> closingskobka = new List<int>();
+                for (int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    char c = textBox1.Text[i];
+                    if (c == '(')
+                    {
+                        stack.Push(i);
+                    }
+                    else if (c == ')')
+                    {
+                        if (stack.Count > 0)
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            closingskobka.Add(i);
+                        }
+                    }
+                }
+                if (stack.Count == 0 && closingskobka.Count == 0)
+                {
+                    MessageBox.Show("Исправлять нечего!");
+                }
+                else
+                {
+                    MessageBox.Show("Скобки не сбалансированы.");
+
+                    foreach(int pos in stack)
+                    {
+                        textBox1.Text = textBox1.Text.Remove(pos, 1);
+                    }
+                    foreach(int pos in closingskobka)
+                    {
+                        textBox1.Text = textBox1.Text.Remove(pos, 1);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите выражение");
+            }
+            File.WriteAllText("text2.txt", textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
